35c9265e8d279fedeca8adb2d95464f69d8e7d03
ITZRAHULPLAYZ <itzrahulplayz@github.com>
2026-05-29 22:56:12 +0530

    Configure for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS for the React frontend
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
